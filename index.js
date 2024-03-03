0b15313c06f2ca010c0fc5dc0e557c99aab9bb83
Jesús López Ansón <839922@unizar.es>
2024-03-03 08:28:53 +0100

    async database query function example

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 // Para instalar los módulos necesarios hacer npm install
 // Ejecutar mediante el comando: node .
+// Hacer "npm run dev" para que se reinicie el servidor automáticamente al guardar cambios
 
 const express = require('express');
 const app = express();
@@ -8,17 +9,23 @@ const PORT = 8080;
 
 const db = require('./db/index'); // Para usar la base de datos
 
+// Ejemplo de función asíncrona para hacer consultas a la base de datos
+
+const aQuery = async (query, params) => {
+    return new Promise((resolve, reject) => {
+        db.query(query, params, (err, res) => {
+            if(err) {
+                reject(err);
+            }
+            resolve(res);
+        });
+    });
+}
+
 app.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
 
     // Ejemplo de consulta a la base de datos
-    // Parámetros: query, parámetros, callback
-    // Callback recibe dos parámetros: error y resultado
-    db.query('SELECT * FROM USUARIOS', [], (err, res) => {
-        if(err) {
-            console.log(err);
-        }
-        console.log(res);
-    });
-
+    const res = await aQuery('SELECT * FROM USUARIOS'); // es opcional pasar parámetros
+    console.log("Resultado ", res);
 });
